5196f3e5cc79026e921b5efc357cd7604c061142
Bexruzbek Hasanbayev <bexruzbekhasanbayev@gmail.com>
2025-12-23 17:57:31 +0500

    qwe

---
+++ app.js
@@ -0,0 +1,7 @@
+// age 18 yoki undan katta bo‘lsa "Voyaga yetgan", aks holda "Voyaga yetmagan" chiqadigan kod yoz.
+let age
+if (age >= 18) {
+  console.log("voyaga etgan");
+} else {
+  console.log("voyaga etmangan");
+}
