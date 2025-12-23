d3c726181d1b96f4c068f56e7497035e1b15fdbc
Bexruzbek Hasanbayev <bexruzbekhasanbayev@gmail.com>
2025-12-23 18:08:33 +0500

    if else

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+//  1. misol
 // age 18 yoki undan katta bo‘lsa "Voyaga yetgan", aks holda "Voyaga yetmagan" chiqadigan kod yoz.
-let age = prompt("Yoshingizni kiriting")
-if (age >= 18) {
-  console.log("voyaga etgan");
-} else {
-  console.log("voyaga etmangan");
-}
+// let age = prompt("Yoshingizni kiriting")
+// if (age >= 18) {
+//   console.log("voyaga etgan");
+// } else {
+//   console.log("voyaga etmangan");
+// }
+
+//  2. misol
+// number musbat yoki manfiy ekanini tekshir.
+
+// let son = prompt("number kriting");
+// if (son % 2 === 0) {
+//   console.log("Musbat son");
+// } else {
+//   console.log("Manfiy son");
+// }
